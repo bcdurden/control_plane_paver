9307998d065048f783e1ebe05ee4bfdacb36d912
Brian Durden <bdurden@pivotal.io>
2020-04-21 13:05:59 -0400

    fixed typo

--- custom_tasks/terraform_init.sh
+++ custom_tasks/terraform_init.sh
@@ -4,7 +4,7 @@
 
 cd platform-automation/terraform
     terraform init
-    terraform plan -out=pcf.plan -var="tls_ca_certificate=$TLS_CA" -var="tls_private_key=$TLS_KEY" -var="tls_wildcard_cert=$TLS_CERT"
+    terraform plan -out=pcf.plan -var="tls_ca_certificate=$TLS_CA" -var="tls_private_key=$TLS_KEY" -var="tls_wildcard_certificate=$TLS_CERT"
     tar czvf ../../terraform-output/terraform.tgz .terraform/
     cp pcf.plan ../../terraform-output
 cd ../../
