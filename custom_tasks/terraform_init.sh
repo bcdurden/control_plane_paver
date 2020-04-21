c1759cd9f7116b0c34f17259f36aa263fd4e5d7a
Brian Durden <bdurden@pivotal.io>
2020-04-21 12:56:02 -0400

    Added tls wildcard cert

--- custom_tasks/terraform_init.sh
+++ custom_tasks/terraform_init.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+# PARAMS: TLS_CA
+echo $TLS_CA
 cd platform-automation/terraform
     terraform init
-    terraform plan -out=pcf.plan
+    terraform plan -out=pcf.plan -var="tls_ca_certificate=$TLS_CA"
     tar czvf ../../terraform-output/terraform.tgz .terraform/
     cp pcf.plan ../../terraform-output
 cd ../../
