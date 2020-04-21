af458e38cec0c9740a76cc4fbf3d4eae959a2c28
Brian Durden <bdurden@pivotal.io>
2020-04-21 13:04:30 -0400

    Updated script

--- custom_tasks/terraform_init.sh
+++ custom_tasks/terraform_init.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # PARAMS: TLS_CA
-echo $TLS_CA
+
 cd platform-automation/terraform
     terraform init
-    terraform plan -out=pcf.plan -var="tls_ca_certificate=$TLS_CA"
+    terraform plan -out=pcf.plan -var="tls_ca_certificate=$TLS_CA" -var="tls_private_key=$TLS_KEY" -var="tls_wildcard_cert=$TLS_CERT"
     tar czvf ../../terraform-output/terraform.tgz .terraform/
     cp pcf.plan ../../terraform-output
 cd ../../
