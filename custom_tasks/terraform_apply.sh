25577fb729e5f05bd0a89d3efc9e578f0e6a1d40
Brian Durden <bdurden@pivotal.io>
2020-04-22 11:14:41 -0400

    Updated script for handling error case

--- custom_tasks/terraform_apply.sh
+++ custom_tasks/terraform_apply.sh
@@ -3,7 +3,9 @@
 tar xvf terraform-output/terraform.tgz -C platform-automation/terraform
 
 cd platform-automation/terraform
-    terraform apply -f pcf.plan
-    rm terraform-output/terraform.tgz
-    tar czvf ../../terraform-output/terraform.tgz .terraform/ pcf.plan terraform.tfstate
+    terraform apply pcf.plan
+    if [ $? == 0 ]; then
+        rm ../../terraform-output/terraform.tgz
+        tar czvf ../../terraform-output/terraform.tgz .terraform/ pcf.plan terraform.tfstate
+    fi
 cd ../../
