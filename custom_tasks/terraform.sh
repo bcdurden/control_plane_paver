d9e25fd8c314cbb3b445b729b9c45aa91a92d1e2
Brian Durden <bdurden@pivotal.io>
2020-05-01 15:55:11 -0400

    Iteration 1

--- custom_tasks/terraform.sh
+++ custom_tasks/terraform.sh
@@ -5,7 +5,7 @@
 export TLS_CA=$(cat tls-certificate/ca.crt)
 export TLS_KEY=$(cat tls-certificate/tls.key)
 export TLS_CERT=$(cat tls-certificate/tls.crt)
-cd platform-automation/terraform
+cd paver-automation/terraform
     terraform init
     terraform plan -out=pcf.plan -var="tls_ca_certificate=$TLS_CA" -var="tls_private_key=$TLS_KEY" -var="tls_wildcard_certificate=$TLS_CERT"
     terraform apply pcf.plan
